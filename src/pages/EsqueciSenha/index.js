68adad4b1b8f9b089132ff68c4bdf7870477d8f1
Rafael-ORibeiro <92262160+Rafael-ORibeiro@users.noreply.github.com>
2021-12-08 00:22:51 -0300

    Create index.js

---
+++ src/pages/EsqueciSenha/index.js
@@ -0,0 +1,9 @@
+export default function EsqueciSenha(){
+
+    return(
+        <div>
+            <h1>Página esqueci senha</h1>
+            <Link to = "/login">Voltar para login</Link>
+        </div>
+    )
+}
