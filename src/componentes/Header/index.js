ad42bf4838505363cbb569bdf9c6f2d913920cbf
Rafael-ORibeiro <92262160+Rafael-ORibeiro@users.noreply.github.com>
2021-12-07 23:54:35 -0300

    Add files via upload

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./index.css";
+
+const Header = () => (
+
+    <div className = "meu-header">
+        Header da minha aplicação
+    </div>
+);
+
+export default Header;
